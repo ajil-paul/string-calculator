0f8fc0c00c0a8b1a4cf96fd3060d85f3b2db9632
Ajil <ajilpaul.268@gmail.com>
2025-07-17 12:53:22 +0530

    Updated rubocop rules to enforce double quotes

--- Gemfile
+++ Gemfile
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-source 'https://rubygems.org'
+source "https://rubygems.org"
 
 # gem "rails"
 
-gem 'rspec', '~> 3.13'
+gem "rspec", "~> 3.13"
 
-gem 'rubocop', '~> 1.78', require: false
+gem "rubocop", "~> 1.78", require: false
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,56 +1,56 @@
 # frozen_string_literal: true
 
-require 'string_calculator'
+require "string_calculator"
 
 RSpec.describe StringCalculator do
-  describe '#add_numbers' do
-    let(:numbers_string) { '1,2' }
+  describe "#add_numbers" do
+    let(:numbers_string) { "1,2" }
     let(:result) { described_class.new.add_numbers(numbers_string) }
 
-    context 'when string is empty' do
-      let(:numbers_string) { '' }
+    context "when string is empty" do
+      let(:numbers_string) { "" }
 
-      it 'returns 0' do
+      it "returns 0" do
         expect(result).to eq(0)
       end
     end
 
-    context 'when string is nil' do
+    context "when string is nil" do
       let(:numbers_string) { nil }
 
-      it 'returns 0' do
+      it "returns 0" do
         expect(result).to eq(0)
       end
     end
 
-    context 'when string contains 2 numbers separated by comma' do
-      let(:numbers_string) { '2,3' }
+    context "when string contains 2 numbers separated by comma" do
+      let(:numbers_string) { "2,3" }
 
-      it 'returns sum of given numbers' do
+      it "returns sum of given numbers" do
         expect(result).to eq(5)
       end
     end
 
-    context 'when string contains only one number' do
-      let(:numbers_string) { '5' }
+    context "when string contains only one number" do
+      let(:numbers_string) { "5" }
 
-      it 'returns that just that number' do
+      it "returns that just that number" do
         expect(result).to eq(5)
       end
     end
 
-    context 'when string contains many numbers separated by comma' do
-      let(:numbers_string) { '2,3,4,5,11,12' }
+    context "when string contains many numbers separated by comma" do
+      let(:numbers_string) { "2,3,4,5,11,12" }
 
-      it 'returns the sum of all numbers' do
+      it "returns the sum of all numbers" do
         expect(result).to eq(37)
       end
     end
 
-    context 'when string contains new lines between numbers instead of commas' do
+    context "when string contains new lines between numbers instead of commas" do
       let(:numbers_string) { "1\n2,3,\n4" }
 
-      it 'should treat the new line as a separator and returns sum of all numbers' do
+      it "should treat the new line as a separator and returns sum of all numbers" do
         expect(result).to eq(10)
       end
     end
